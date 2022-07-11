fdfc6a1ab120f0050e9860a094c5b139dcdafc03
kaarthi <Kaarthikdhanushka2004@gmail.com>
2022-07-11 13:01:27 +0530

    added

--- src/org/test/A.java
+++ src/org/test/A.java
@@ -3,9 +3,11 @@ package org.test;
 public class A {
 private void StudentId() {
 System.out.println("student id is 22");
+System.out.println("student status is pass");
 }
 private void StudentName() {
 System.out.println("student name is dhanu");
+System.out.println("student school is KSR");
 }
 public static void main(String[] args) {
 	A a=new A();
