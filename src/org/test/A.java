36fac5524f2649b917fbbd2712c77d45da2e9b44
kaarthi <Kaarthikdhanushka2004@gmail.com>
2022-07-11 12:36:22 +0530

    added

---
+++ src/org/test/A.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class A {
+private void StudentId() {
+System.out.println("student id is 22");
+}
+private void StudentName() {
+System.out.println("student name is dhanu");
+}
+public static void main(String[] args) {
+	A a=new A();
+	a.StudentId();
+	a.StudentName();
+}
+}
